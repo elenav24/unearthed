dee802e656f1e1c899b46945ca26693bd70db89e
elena marquez <elena-marquez@csu.fullerton.edu>
2026-02-25 18:59:31 -0800

    completed lab1

---
+++ server/data/gifts.js
@@ -0,0 +1,104 @@
+const giftData = [
+  {
+    id: 1,
+    name: "Mini Desktop Vacuum",
+    pricePoint: "$",
+    audience: "Students",
+    image: "https://www.ttpen.com/cdn/shop/products/41Kl_ApgqbS._AC_SL1050_1a662d72-5c2f-4351-9e60-98f40a02203c.jpg",
+    description: "A tiny USB-powered vacuum perfect for cleaning crumbs off desks and keyboards.",
+    submittedBy: "Elena",
+    submittedOn: "2026-02-25"
+  },
+  {
+    id: 2,
+    name: "Galaxy Light Projector",
+    pricePoint: "$",
+    audience: "Teens & Young Adults",
+    image: "https://m.media-amazon.com/images/I/811dAEhf3cL._AC_SL1500_.jpg",
+    description: "Projects colorful stars and nebula effects onto walls and ceilings for a relaxing vibe.",
+    submittedBy: "Jordan",
+    submittedOn: "2026-02-20"
+  },
+  {
+    id: 3,
+    name: "Self-Heating Coffee Mug",
+    pricePoint: "$$$",
+    audience: "Coffee Lovers",
+    image: "https://m.media-amazon.com/images/I/61r3HjRPLvL._AC_UF894,1000_QL80_.jpg",
+    description: "Keeps drinks warm for hours using a rechargeable heating base.",
+    submittedBy: "Avery",
+    submittedOn: "2026-02-18"
+  },
+  {
+    id: 4,
+    name: "Custom Photo Puzzle",
+    pricePoint: "$$",
+    audience: "Couples & Families",
+    image: "https://sfly3-catalog-design-imzr.sf-cdn.com/externalapy6m8iu/556/2024/11/21/021408/PUZZLE-193052-12015-MERCHLARGE_FRONT?_t=1761661312429",
+    description: "Turn a favorite photo into a personalized jigsaw puzzle.",
+    submittedBy: "Maya",
+    submittedOn: "2026-02-15"
+  },
+  {
+    id: 5,
+    name: "Bluetooth Shower Speaker",
+    pricePoint: "$$",
+    audience: "Music Fans",
+    image: "https://c1.neweggimages.com/productimage/nb640/BMC3D2309250851RFC0.jpg",
+    description: "Water-resistant speaker that sticks to shower walls for singing along.",
+    submittedBy: "Chris",
+    submittedOn: "2026-02-12"
+  },
+  {
+    id: 6,
+    name: "Plant Growing Kit",
+    pricePoint: "$$",
+    audience: "Plant Beginners",
+    image: "https://m.media-amazon.com/images/I/810-YD6IQmL.jpg",
+    description: "An easy starter kit to grow herbs or small plants indoors.",
+    submittedBy: "Sam",
+    submittedOn: "2026-02-10"
+  },
+  {
+    id: 7,
+    name: "Polaroid Style Instant Camera",
+    pricePoint: "$$$",
+    audience: "Photographers",
+    image: "https://m.media-amazon.com/images/I/71a0-JzaiEL.jpg",
+    description: "Print photos instantly for scrapbooks and memories.",
+    submittedBy: "Taylor",
+    submittedOn: "2026-02-08"
+  },
+  {
+    id: 8,
+    name: "Funny Novelty Socks Set",
+    pricePoint: "$",
+    audience: "Anyone",
+    image: "https://m.media-amazon.com/images/I/91+2JYUyeKL._AC_UY1000_.jpg",
+    description: "Colorful themed socks with fun patterns and jokes.",
+    submittedBy: "Riley",
+    submittedOn: "2026-02-05"
+  },
+  {
+    id: 9,
+    name: "Magnetic Levitating Globe",
+    pricePoint: "$$$",
+    audience: "Tech Lovers",
+    image: "https://m.media-amazon.com/images/I/6182waIcsJL._AC_UF350,350_QL80_.jpg",
+    description: "A futuristic globe that floats in midair using magnetic levitation.",
+    submittedBy: "Alex",
+    submittedOn: "2026-02-03"
+  },
+  {
+    id: 10,
+    name: "DIY Candle Making Kit",
+    pricePoint: "$$",
+    audience: "Creative Hobbyists",
+    image: "https://shop.pamm.org/cdn/shop/files/Untitled-1-2025-09-19T120918.020.png?v=1758298164",
+    description: "Includes wax, scents, and molds to create custom candles at home.",
+    submittedBy: "Jamie",
+    submittedOn: "2026-02-01"
+  }
+];
+
+export default giftData;
